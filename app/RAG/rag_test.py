8e5c78da2a49a917d83155d1af5098f422ba1a20
DongHun Choi <cdh3946@gmail.com>
2026-02-02 22:14:36 +0900

    RAG 사전 키워드 작성

--- app/RAG/rag_test.py
+++ app/RAG/rag_test.py
@@ -7,7 +7,8 @@ from langchain_upstage import ChatUpstage, UpstageEmbeddings
 from langchain_chroma import Chroma
 from langchain import hub
 from langchain.chains import RetrievalQA
-
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 
 text_splitter = RecursiveCharacterTextSplitter(
@@ -15,7 +16,7 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=200
 )
 
-loader = Docx2txtLoader("./tax.docx")
+loader = Docx2txtLoader("./tax_with_markdown.docx")
 document_list = loader.load_and_split(text_splitter=text_splitter)
 
 upstage_embedding = UpstageEmbeddings(
@@ -23,20 +24,20 @@ upstage_embedding = UpstageEmbeddings(
     model="solar-embedding-1-large"
 )
 
-# # 데이터 처음 저장할 때
-# database = Chroma.from_documents(
-#     documents=document_list,
-#     embedding=upstage_embedding,
-#     collection_name="chroma-tax",
-#     persist_directory="./chroma"
-# )
+# 데이터 처음 저장할 때
+database = Chroma.from_documents(
+    documents=document_list,
+    embedding=upstage_embedding,
+    collection_name="chroma-tax",
+    persist_directory="./chroma"
+)
 
 # # 이미 저장된 데이터 사용할 때
-database = Chroma(
-    collection_name="chroma-tax",
-    persist_directory="./chroma",
-    embedding_function=upstage_embedding
-)
+# database = Chroma(
+#     collection_name="chroma-tax",
+#     persist_directory="./chroma",
+#     embedding_function=upstage_embedding
+# )
 
 query = "연봉 5천만원인 직장인의 소득세는 얼마인가요?"
 retrieved_docs = database.similarity_search(query, k=3)
@@ -56,4 +57,25 @@ qa_chain = RetrievalQA.from_chain_type(
 )
 
 ai_message = qa_chain.invoke({"query": query})
-print(ai_message)
+# print(ai_message["result"])
+
+dictionary = ["사람을 나타내는 표현 -> 거주자"]
+prompt1 = ChatPromptTemplate.from_template(f"""
+    사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
+    만약 변경할 필요가 없다고 판단된다면, 사용자의 질문을 변경하지 않아도 됩니다.
+
+    사전 : {dictionary}
+    
+    질문 : {{question}}
+""")
+
+dictionary_chain = prompt1 | llm | StrOutputParser()
+
+# new_question = dictionary_chain.invoke({"question": query})
+# print(new_question)
+
+tax_chain = {"query": dictionary_chain} | qa_chain
+
+ai_response = tax_chain.invoke({"question": query})
+
+print(ai_response)
